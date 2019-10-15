5159aa4e8416cf637c33c6fa7ffd81c838a9f20b
jpapis1 <jpapis1@gmail.com>
2019-10-15 17:24:47 +0200

    Initial commit.

---
+++ src/test/java/projektzes/cardscraft/CardscraftApplicationTests.java
@@ -0,0 +1,13 @@
+package projektzes.cardscraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardscraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
